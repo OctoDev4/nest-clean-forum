6a13e7fdbff2838fdbfa4e36b641cf8b4355191b
Felipe Almeida <felipe.alhopato@gmail.com>
2024-05-20 20:54:32 -0300

    controllers e2e testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import {envSchema} from "./env";
 import {AuthModule} from "./auth/auth.module";
 import {AuthenticateController} from "./controllers/authenticate.controller";
 import {CreateQuestionController} from "./controllers/create-question.controller";
-import {FetchRecentQuestionsController} from "./controllers/fetch-recents.controller";
+import {FetchRecentQuestionsController} from "./controllers/fetch-recents-questions.controller";
 
 
 @Module({
